f6e136c39eb0031904b6113314669765f8dcb038
Anderson Brasil <andersonbrasil@gmail.com>
2024-02-23 10:06:55 -0300

    Push230220241006

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,4 +2,5 @@ import { Router } from "express";
 import { veroController } from "../http/controllers/VeroStoreController";
 export const router = Router();
 
+router.get('/', (req, res, next) => next('API Vero Store funcionando corretamente'));
 router.post("/verostore", veroController.getVeroStore);
